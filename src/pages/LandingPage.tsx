9ffbf6ca6e66a52b4ebba6a2d9579d00b7374523
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-01-28 14:46:56 +0800

    Created initial landing page style

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,25 @@
+import { CornerRightDown } from 'lucide-react'
 import React from 'react'
 
 const LandingPage = () => {
-  return <div>LandingPage</div>
+  return (
+    <div className='flex h-full w-full justify-center p-12 py-16'>
+      <div className='card bg-base-100 flex w-1/2 flex-col gap-8 rounded-lg px-12 py-8 shadow-xl'>
+        <h2 className='card-title'>Hello Weather!</h2>
+        <div className='flex gap-2'>
+          <p>
+            Login using your Github account to use the app through the button
+            below.
+          </p>
+          <CornerRightDown />
+        </div>
+
+        <div className='card-actions'>
+          <button className='btn btn-accent'>Login</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default LandingPage
