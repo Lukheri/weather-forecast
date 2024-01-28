b30d2c84720eeb064512059ea699a6eb6b8ecf28
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-01-28 14:16:12 +0800

    Updated Navbar UI and functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
+import { CloudSun } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='bg-base-300 flex items-center justify-between p-5 py-3'>
+      <div className='flex items-center gap-3'>
+        <Link to='/'>
+          <CloudSun size={40} />
+        </Link>
+        <Link to='/'>Landing page</Link>
+        <Link to='/home'>Home</Link>
+        <Link to='/weather'>Weather</Link>
+      </div>
+      <div>
+        <button className='btn btn-accent'>Logout</button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
